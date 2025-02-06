127c6f3af518c4e18970e28be14f800c8e1c8c6d
kolotw <77420523+kolotw@users.noreply.github.com>
2025-02-06 08:36:07 +0800

    0206

---
+++ Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class UI_Controller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnEnable()
+    {
+        // 獲取根元素
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        // 找到按鈕元素
+        Button startButton = root.Q<Button>("But_Start");
+        Button mapButton = root.Q<Button>("But_Map");
+
+        // 註冊按鈕點擊事件
+        startButton.clicked += () => { SceneManager.LoadScene("Level"); };
+        mapButton.clicked += () => { SceneManager.LoadScene("Level"); };
+    }
+}
